43d0473575802adae1db4d3b19cb6a74d85b40b8
webdmitriev <webdmitriev@gmail.com>
2019-10-10 14:34:09 +0300

    no message

---
+++ .nuxt/dist/server/d9ff7ec8727714291bed.js
@@ -0,0 +1,2 @@
+exports.ids=[8],exports.modules={235:function(t,e,o){"use strict";o.r(e);o(5);var r={layout:"admin",computed:{postsLoaded(){}}},n=o(1);var component=Object(n.a)(r,function(){var t=this.$createElement;return(this._self._c||t)("h1",[this._ssrNode("Админ панель")])},[],!1,function(t){},null,"6e38415e");e.default=component.exports}};
+//# sourceMappingURL=d9ff7ec8727714291bed.js.map
--- .nuxt/dist/server/ee5e51fa74ca5383c44d.js
+++
@@ -1,2 +0,0 @@
-exports.ids=[8],exports.modules={232:function(t,e,o){"use strict";o.r(e);o(5);var r={layout:"admin",computed:{postsLoaded(){}}},n=o(1);var component=Object(n.a)(r,function(){var t=this.$createElement;return(this._self._c||t)("h1",[this._ssrNode("Админ панель")])},[],!1,function(t){},null,"6e38415e");e.default=component.exports}};
-//# sourceMappingURL=ee5e51fa74ca5383c44d.js.map
